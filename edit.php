624ccf41419c41efdba842dae909e29691f9ff16
Tekkun-0116 <tekkun.2358@gmail.com>
2020-03-30 11:20:01 +0900

    リファクタリング完了

--- edit.php
+++ edit.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once('config.php');
+require_once('categories.php');
 require_once('functions.php');
+require_once('posts.php');
 
 session_start();
 
@@ -11,13 +12,7 @@ if (!is_numeric($id)) {
   exit;
 }
 
-$dbh = connectDb();
-
-$sql = "select * from posts where id = :id";
-
-$stmt = $dbh->prepare($sql);
-$stmt->bindParam(":id", $id, PDO::PARAM_INT);
-$stmt->execute();
+$post = getPostFindById($id);
 
 $post = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -26,53 +21,14 @@ if (empty($post)) {
   exit;
 }
 
-$sql = "select * from categories";
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-
-$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$categories = getAllCategories();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $title = $_POST['title'];
-  $body = $_POST['body'];
-  $category_id = $_POST['category_id'];
-
-  $errors = [];
-
-  if ($title == '') {
-    $errors = 'タイトルが未入力です';
-  }
-  if ($category_id == '') {
-    $errors = 'カテゴリーが未入力です';
-  }
-  if ($body == '') {
-    $errors = '本文が未入力です';
-  }
-
-  if (empty($errors)) {
-    $sql = <<<SQL
-    update
-      posts
-    set
-      title = :title,
-      body = :body,
-      category_id = :category_id
-    where
-      id = :id
-    SQL;
-    
-    $stmt = $dbh->prepare($sql);
-
-    $stmt->bindParam(':title', $title, PDO::PARAM_STR);
-    $stmt->bindParam(':body', $body, PDO::PARAM_STR);
-    $stmt->bindParam(':category_id', $category_id, PDO::PARAM_INT);
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-
-    $stmt->execute();
+  $errors = inputChkPost($_POST);
+    updatePost($_POST);
     header("Location: show.php?id={$id}");
     exit;
   }
-}
 
 ?>
 
